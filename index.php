4e66b601b028675b961890642785e2aacbccbd3a
Sergey Maximov <sergey@dashtickets.com>
2015-02-17 19:11:03 +1300

    Printing Numbers from 1 to 100

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+$numbers = array();
+$i=1;
+
+while($i<100) {
+	if(is_int($i/3) && is_int($i/5)) {
+		echo 'FizzBuzz' . '</br>';
+		$i++;
+	}
+	if(is_int($i/3))
+	{
+		echo 'Fizz' . '</br>';
+		$i++;
+	}
+	if(is_int($i/5)){
+		echo 'Bazz' . '</br>';
+		$i++;
+	}
+	else{
+		echo $i . '</br>';
+		$i++;
+	}
+
+}
+
+
+
+
+?>
